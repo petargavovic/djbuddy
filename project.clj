c2f8ccf76c9000cad93e52cf9ace4ff86cf50ac3
petargavovic <stoniminetar@gmail.com>
2026-08-07 00:52:13 +0200

    Project creation

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject djbuddy "0.1.0-SNAPSHOT"
+  :description "Stats for your DJ sets!"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [clj-http "3.12.3"]
+                 [cheshire "5.11.0"]]
+  :source-paths ["src"]
+  :plugins [[lein-cljsbuild "1.1.1"]]
+
+  :profiles {:dev {:dependencies [[midje "1.10.6"]]
+                   :plugins [[lein-midje "3.2.2"]]}})
