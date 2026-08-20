d9f077da62afbd362e4e5b48fef483deb0932b56
petargavovic <stoniminetar@gmail.com>
2026-08-20 05:59:55 +0200

    Improved history prompt

--- src/djbuddy/vdj/history.clj
+++ src/djbuddy/vdj/history.clj
@@ -2,6 +2,8 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str])
   (:import [java.time Instant LocalTime]
+           [java.time.format DateTimeFormatterBuilder]
+           [java.time LocalDate LocalDateTime LocalTime]
            [java.time.format DateTimeFormatterBuilder]))
 
 (def history-dir
@@ -62,11 +64,66 @@
        :duration-seconds song-length
        :source          :vdj-history})))
 
-(defn read-history [file]
-  (with-open [reader (io/reader file)]
-    (->> (line-seq reader)
-         (keep parse-history-line)
-         doall)))
+(defn read-history [file date]
+  (let [played-date (LocalDate/parse date)]
+
+    (with-open [reader (io/reader file)]
+      (->> (line-seq reader)
+
+           (keep parse-history-line)
+
+           (map
+             (fn [track]
+               (assoc track
+                 :played-date played-date
+                 :played-date-time
+                 (LocalDateTime/of
+                   played-date
+                   (:played-time track)))))
+
+           doall
+           vec))))
+
+(defn crosses-midnight? [start-time end-time]
+  (.isAfter
+    (parse-time start-time)
+    (parse-time end-time)))
+
+(defn set-range [date start-time end-time]
+  (let [start-date (LocalDate/parse date)
+
+        start
+        (LocalDateTime/of
+          start-date
+          (parse-time start-time))
+
+        end-date
+        (if (crosses-midnight? start-time end-time)
+          (.plusDays start-date 1)
+          start-date)
+
+        end
+        (LocalDateTime/of
+          end-date
+          (parse-time end-time))]
+
+    {:start start
+     :end end
+     :end-date end-date}))
+
+(defn datetime-between? [datetime start end]
+  (and
+    (not (.isBefore datetime start))
+    (not (.isAfter datetime end))))
+
+(defn tracks-between-datetimes [tracks start end]
+  (->> tracks
+       (filter
+         #(datetime-between?
+            (:played-date-time %)
+            start
+            end))
+       vec))
 
 (defn time-between? [time start end]
   (if (<= (compare start end) 0)
@@ -98,10 +155,65 @@
     (range)
     tracks))
 
-(defn import-set [history-file start-time end-time]
-  (-> (read-history history-file)
-      (tracks-between start-time end-time)
-      (add-track-order)))
+(defn history-file-for-date [date]
+  (let [date     (LocalDate/parse date)
+        filename (str date ".m3u")
+        year     (str (.getYear date))
+        month    (format "%02d" (.getMonthValue date))
+
+        direct
+        (io/file history-dir filename)
+
+        archived
+        (io/file history-dir year month filename)]
+
+    (cond
+      (.exists direct)
+      direct
+
+      (.exists archived)
+      archived
+
+      :else
+      nil)))
+
+(defn import-set [date start-time end-time]
+  (let [{:keys [start end end-date]}
+        (set-range date start-time end-time)
+
+        start-date
+        (LocalDate/parse date)
+
+        dates
+        (if (= start-date end-date)
+          [start-date]
+          [start-date end-date])
+
+        tracks
+        (mapcat
+          (fn [current-date]
+            (let [date-string (str current-date)]
+
+              (if-let [file
+                       (history-file-for-date
+                         date-string)]
+
+                (read-history
+                  file
+                  date-string)
+
+                (do
+                  (println
+                    "VirtualDJ history file not found for:"
+                    date-string)
+                  []))))
+
+          dates)]
+
+    (-> tracks
+        (tracks-between-datetimes start end)
+        add-track-order)))
+
 
 (defn track-by-order [tracks order]
   (first
